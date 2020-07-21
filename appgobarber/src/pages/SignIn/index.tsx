35dc1a91d60a392dff13c939b98e63c81bb2cb9b
WallaceGomes <wallacecardosogomes@gmail.com>
2020-07-21 19:56:16 -0300

    chore(pages): SignIn page layout

--- appgobarber/src/pages/SignIn/index.tsx
+++ appgobarber/src/pages/SignIn/index.tsx
@@ -1,31 +1,85 @@
 import React from 'react';
-import { Image } from 'react-native';
+import {
+  Image,
+  KeyboardAvoidingView,
+  ScrollView,
+  Platform,
+  View,
+} from 'react-native';
+import Icon from 'react-native-vector-icons/Feather';
 
 import Input from '../../components/Input';
 import Button from '../../components/Button';
 
 import logoImg from '../../assets/logo.png';
 
-import { Container, Title } from './styles';
+import {
+  Container,
+  Title,
+  ForgotPassword,
+  ForgotPasswordText,
+  CreateAccountButton,
+  CreateAccountButtonText,
+} from './styles';
+
+/* NOTA
+
+  No IOS o teclado entra na frente da tela quando vai digitar então tem que usar o
+  keyboardAvoidingView para isso não acontecer
+  No Android esse comportamente já o padrão xD
+
+  Os Textos no RN não possuem animação, pode-se usar uma tag View em branco
+  para fazer a animação nela e consequentemente no texto...
+
+  ScrollView é usado para abilitar o scroll na tela
+  keyboardShouldPersistTaps > Comportamento do teclado quando tocar na parte de fora dele,
+  handled > baseado no SO
+*/
 
 const SignIn: React.FC = () => {
   return (
-    <Container>
-      <Image source={logoImg}></Image>
-
-      <Title>Faça seu logon</Title>
-
-      <Input name="email" icon="mail" placeholder="E-mail" />
-      <Input name="password" icon="lock" placeholder="Senha" />
-
-      <Button
-        onPress={() => {
-          console.log('ok');
-        }}
+    <>
+      <KeyboardAvoidingView
+        style={{ flex: 1 }}
+        behavior={Platform.OS === 'ios' ? 'padding' : undefined}
+        enabled
       >
-        Entrar
-      </Button>
-    </Container>
+        <ScrollView
+          contentContainerStyle={{ flex: 1 }}
+          keyboardShouldPersistTaps="handled"
+        >
+          <Container>
+            <Image source={logoImg}></Image>
+            <View>
+              <Title>Faça seu logon</Title>
+            </View>
+
+            <Input name="email" icon="mail" placeholder="E-mail" />
+            <Input name="password" icon="lock" placeholder="Senha" />
+
+            <Button
+              onPress={() => {
+                console.log('ok');
+              }}
+            >
+              Entrar
+            </Button>
+
+            <ForgotPassword
+              onPress={() => {
+                console.log('ok');
+              }}
+            >
+              <ForgotPasswordText>Esqueci minha senha</ForgotPasswordText>
+            </ForgotPassword>
+          </Container>
+        </ScrollView>
+      </KeyboardAvoidingView>
+      <CreateAccountButton>
+        <Icon name="log-in" size={20} color="#ff9000" />
+        <CreateAccountButtonText>Criar uma conta</CreateAccountButtonText>
+      </CreateAccountButton>
+    </>
   );
 };
 
